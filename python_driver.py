86e3c55d3df7fd193b3030d6c28b26ac51cdd9f1
qcha41 <q.chateiller@gmail.com>
2019-10-12 16:03:48 +0200

    up

--- python_driver.py
+++ python_driver.py
@@ -1,13 +1,27 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
+"""
+Supported instruments (identified):
+- 
+"""
 
+import numpy as np
+import visa
+    
 
 class Driver():
     
     category = 'Optical shutter'
     
-    def __init__(self):
+    def __init__(self,address='ASRL::2::INSTR'):
+        
+        # Instantiation
+        rm = visa.ResourceManager()
+        self.inst = rm.open_resource(address)
+        self.inst.timeout = 5000 #ms
+        self.inst.baud_rate = 115200 
+
         
         # Angle configuration : 1=Closed  0=Open
         self.POS = {1:{1:30,0:60},
@@ -17,9 +31,29 @@ class Driver():
         self.config = '111' # default position ALL closed
 
 
+    def close(self):
+        try : self.inst.close()
+        except : pass
+
+    def query(self,command):
+
+        result = self.inst.query(command)
+        result = result.strip('\n')
+        
+        if '=' in result : result = result.split('=')[1]
+        
+        try : result = float(result)
+        except: pass
+    
+        return result
+    
+    def write(self,command):
+        self.inst.write(command)
+        
+
 
     def getID(self):
-        return self.query('*IDN?')
+        return self.query('ID?')
     
 
 
@@ -38,7 +72,7 @@ class Driver():
         for i in range(3):
             try :
                 servoID = i+1
-                position = self.shutterPositions[servoID][int(value[i])]
+                position = self.POS[servoID][int(value[i])]
                 command+=f'SRV{servoID}={position},'
             except :
                 pass
@@ -60,6 +94,7 @@ class Driver():
                 newConfig += str(int(not bool(int(self.config[i]))))
             except :
                 newConfig += 'x'
+
         self.setConfig(newConfig)
         
       
@@ -73,63 +108,36 @@ class Driver():
     def setAngleShutter3(self,value):
         self.query(f'SRV3={value}')
         
-    def getDriverConfig(self):
         
-        config = []
+    def setShutter1(self,value):
+        self.setConfig(f'{int(value)}xx')
         
-        config.append({'element':'action','name':'closeAll','do':self.setSafeState,'help':'Close every shutters'})
-        config.append({'element':'action','name':'invert','do':self.invertConfig,'help':'Invert every shutters state'})
-        config.append({'element':'variable','name':'config','read':self.getConfig,'write':self.setConfig,'type':str,'help':'Shutter configuration. 1 is closed, 0 is opened.'})
-
-        return config
+    def setShutter2(self,value):
+        self.setConfig(f'x{int(value)}x')
+        
+    def setShutter3(self,value):
+        self.setConfig(f'xx{int(value)}')
+        
+        
+    def getShutter1(self):
+        return bool(int(self.config[0]))
+    
+    def getShutter2(self):
+        return bool(int(self.config[1]))
+    
+    def getShutter3(self):
+        return bool(int(self.config[2]))
+    
+    
+       
+    
+    def danse(self):
+        mess = ''
+        for i in range(6) :
+            for j in range(1,4) :
+                mess += f'SRV{j}={round(np.random.random()*180)},'
+        self.inst.timeout = 10000
+        self.query(mess)
+        self.inst.timeout = 5000
   
-
-
-#################################################################################
-############################## Connections classes ##############################
-class Driver_VISA(Driver):
-    def __init__(self, address='ASRL::2::INSTR', **kwargs):
-        import visa 
-        
-        self.DEF_TIMEOUT = 1000 #ms
-        self.LONG_TIMEOUT = 5000 #ms
-        self.BAUDRATE = 115200 
-        
-        # Instantiation
-        rm = visa.ResourceManager()
-        self.inst = rm.open_resource(address)
-        self.inst.timeout = self.DEF_TIMEOUT
-        self.inst.baud_rate = self.BAUDRATE
-        
-        Driver.__init__(self)
-        
-        
-    def close(self):
-        try : self.inst.close()
-        except : pass
-
-    def query(self,command):
-        result = self.inst.query(command)
-        result = result.strip('\n')
-        
-        if '=' in result : result = result.split('=')[1]
-        
-        if 'SRV' in command : self.inst.timeout = self.LONG_TIMEOUT
-        else : self.inst.timeout = self.DEF_TIMEOUT
-        
-        try : result = float(result)
-        except: pass
     
-        return result
-    
-    def write(self,command):
-        self.inst.write(command)
-
-############################## Connections classes ##############################
-#################################################################################
-        
-        
-        
-if __name__ == '__main__' : 
-    ADDRESS = 'ASRL15::INSTR'
-        
